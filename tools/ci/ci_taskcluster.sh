104e3e0399a7bd31e521b0b2b3340bb1b7c52b95
James Graham <james@hoppipolla.co.uk>
2018-01-17 11:03:01 +0000

    Don't prompt for install

--- tools/ci/ci_taskcluster.sh
+++ tools/ci/ci_taskcluster.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 pip install virtualenv
-python2 wpt run $1 --log-tbpl=- --log-tbpl-level=debug --log-wptreport=wpt_report.json --this-chunk=$3 --total-chunks=$4 --test-type=$2
+python2 wpt run $1 --log-tbpl=- --log-tbpl-level=debug --log-wptreport=wpt_report.json --this-chunk=$3 --total-chunks=$4 --test-type=$2 -y
