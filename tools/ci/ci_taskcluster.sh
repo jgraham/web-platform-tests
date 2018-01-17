853387ea91a39fc712487f0692e609291ce06db1
James Graham <james@hoppipolla.co.uk>
2018-01-17 11:44:56 +0000

    Install dependencies

--- tools/ci/ci_taskcluster.sh
+++ tools/ci/ci_taskcluster.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-pip install virtualenv
+# To make sure we have all the deps installed
+sudo apt install firefox
+
+pip -q install virtualenv
 python2 wpt run $1 --log-tbpl=- --log-tbpl-level=debug --log-wptreport=wpt_report.json --this-chunk=$3 --total-chunks=$4 --test-type=$2 -y --install-browser
