3bd70ad19d1ec22e0b620c89d7d783532310fb1d
James Graham <james@hoppipolla.co.uk>
2018-01-17 11:19:34 +0000

    Use  sys.executable for virtualenv

--- tools/wpt/virtualenv.py
+++ tools/wpt/virtualenv.py
@@ -22,7 +22,7 @@ class Virtualenv(object):
     def create(self):
         if os.path.exists(self.path):
             shutil.rmtree(self.path)
-        call(self.virtualenv, self.path)
+        call(self.virtualenv, self.path, "-p", sys.executable)
 
     @property
     def bin_path(self):
