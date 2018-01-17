08864cb52490e9f20ce372dbc27e5e9123d60184
James Graham <james@hoppipolla.co.uk>
2018-01-17 14:30:43 +0000

    Don't update

--- tools/ci/ci_taskcluster.sh
+++ tools/ci/ci_taskcluster.sh
@@ -3,8 +3,8 @@
 set -xe
 
 # To make sure we have all the deps installed
-apt-get --yes update
-apt-get --yes upgrade
+#apt-get --yes update
+#apt-get --yes upgrade
 
 pip -q install virtualenv
 python2 wpt run $1 --log-tbpl=- --log-tbpl-level=debug --log-wptreport=wpt_report.json --this-chunk=$3 --total-chunks=$4 --test-type=$2 -y --install-browser
