cd1ed8e8e49fe63c6487f2d0db26950a2e6c01b0
James Graham <james@hoppipolla.co.uk>
2018-01-24 21:45:21 +0000

    Upload artifacts and change wpt run exit code

--- tools/wpt/run.py
+++ tools/wpt/run.py
@@ -397,7 +397,8 @@ def run(venv, **kwargs):
 
 def run_single(venv, **kwargs):
     from wptrunner import wptrunner
-    return wptrunner.start(**kwargs)
+    wptrunner.start(**kwargs)
+    return
 
 
 def main():
