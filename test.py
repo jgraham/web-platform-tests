91459d8267aeb9ad9e866c6875a48734a1dd750e
James Graham <james@hoppipolla.co.uk>
2021-04-01 20:49:36 +0100

    Make test script launch Firefox (path to binary as first arg to script)

--- test.py
+++ test.py
@@ -1,14 +1,65 @@
-import requests
 import asyncio
+import re
+import subprocess
+import sys
+
+import requests
 
 from tools import localpaths
 
 import webdriver
 
-async def main():
-    caps = requests.post("http://localhost:9222/session", json={"capabilities": {"alwaysMatch": {"websocketUrl": True}}}).json()
-    async with webdriver.bidi.BidiSession("ws://localhost:9222/", caps, session_id=caps["sessionId"]) as session:
-        resp = await session.session.test({"example": "data"})
-        print(await resp)
+async def read_firefox(proc, future):
+    url_re = re.compile(".*WebDriver BiDi listening on ([^ ]*)")
+    while True:
+        line = await proc.stdout.readline()
+        if line == b'':
+            break
+        line = line.decode("utf-8", "replace")
+        if not future.done():
+            m = url_re.match(line)
+            if m:
+                url = m.groups(1)[0]
+                print("Got BiDi URL %s" % url)
+                url = url.strip()
+                url = url.replace("ws://", "")
+                future.set_result(url)
+        else:
+            if "BiDi" in line or "RemoteAgent" in line:
+                print(line)
+    print("No more output")
 
-asyncio.get_event_loop().run_until_complete(main())
+
+async def run_bidi_session(loop, url):
+    print("Starting session using url %s" % url)
+    caps = requests.post("http://%s/session" % url, json={"capabilities": {"alwaysMatch": {"websocketUrl": True}}}).json()
+    async with webdriver.bidi.BidiSession("ws://%s/" % url, caps, session_id=caps["sessionId"]) as session:
+        test = webdriver.bidi.client.Test(session)
+        result = await test.test_method(example="data")
+        print("Result:", result)
+
+
+async def main():
+    loop = asyncio.get_event_loop()
+    future = loop.create_future()
+
+    print("Starting Gecko")
+    proc = await asyncio.create_subprocess_exec(
+        sys.argv[1], "--remote-debugging-port",
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.STDOUT)
+
+    fx_task = asyncio.create_task(read_firefox(proc, future))
+    try:
+        url = await future
+
+        await loop.create_task(run_bidi_session(loop, url))
+    finally:
+        print("Killing process")
+        proc.terminate()
+        await proc.wait()
+        await fx_task
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
